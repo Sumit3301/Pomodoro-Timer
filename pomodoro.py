ce7fa462b8ecc9667224cf2a27ac4b380dff8bc4
Sumit Gaur <sumit.gaur1999@gmail.com>
2021-08-27 01:08:19 +0530

    setting up canvas

---
+++ pomodoro.py
@@ -0,0 +1,16 @@
+import tkinter
+from tkinter import *
+FONT="Courier"
+WORK_TIME=25
+SHORT_BREAK=5
+LONG_BREAK=10
+
+
+window=Tk()
+
+window.title("Pomodoro")
+
+canvas=window.Canvas(width=100,height=100)
+canvas.create_image(100,112,image=)
+
+window.mainloop()
