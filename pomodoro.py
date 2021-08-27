93153265c72703325e776728860b87b82d1a0b7e
Sumit Gaur <sumit.gaur1999@gmail.com>
2021-08-27 21:22:51 +0530

    added image

--- pomodoro.py
+++ pomodoro.py
@@ -1,5 +1,7 @@
 import tkinter
 from tkinter import *
+from tkinter import font
+
 FONT="Courier"
 WORK_TIME=25
 SHORT_BREAK=5
@@ -10,7 +12,10 @@ window=Tk()
 
 window.title("Pomodoro")
 
-canvas=window.Canvas(width=100,height=100)
-canvas.create_image(100,112,image=)
+canvas=Canvas(width=250,height=250)
+pomodoro=PhotoImage(file="tomato.png")
+canvas.create_image(120,120, image=pomodoro)
+canvas.create_text(120,110,text="00:00",fill="white",font=("Arial",25,"bold"))
+canvas.pack()
 
 window.mainloop()
