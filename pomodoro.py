64995f2f225c318c03e25221b73f1fd3d3f1ea3c
Sumit Gaur <sumit.gaur1999@gmail.com>
2021-09-01 01:48:56 +0530

    added buttons

--- pomodoro.py
+++ pomodoro.py
@@ -13,12 +13,20 @@ window=Tk()
 window.title("Pomodoro")
 window.config(padx=50,pady=50,bg="YELLOW")
 
-title_label=Label(text="Timer",fg="GREEN")
+title_label=Label(text="Timer",fg="GREEN",bg="YELLOW")
+
 title_label.grid(column=1,row=0)
+title_label.config(font=('Helvatical bold',40))
 
 canvas=Canvas(width=250,height=250)
 pomodoro=PhotoImage(file="tomato.png")
 canvas.create_image(120,120, image=pomodoro)
 canvas.create_text(120,110,text="00:00",fill="white",font=("Arial",25,"bold"))
+canvas.grid(column=1,row=2)
+start=Button(text="Start")
+start.grid(column=0,row=3)
+
+reset=Button(text="reset")
+reset.grid(column=3,row=3)
 
 window.mainloop()
