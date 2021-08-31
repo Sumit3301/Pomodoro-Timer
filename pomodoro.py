d3f5d76e474e2bca15a3c600d278b9ce731ae46f
Sumit Gaur <sumit.gaur1999@gmail.com>
2021-08-31 23:57:09 +0530

    added title

--- pomodoro.py
+++ pomodoro.py
@@ -11,11 +11,14 @@ LONG_BREAK=10
 window=Tk()
 
 window.title("Pomodoro")
+window.config(padx=50,pady=50,bg="YELLOW")
+
+title_label=Label(text="Timer",fg="GREEN")
+title_label.grid(column=1,row=0)
 
 canvas=Canvas(width=250,height=250)
 pomodoro=PhotoImage(file="tomato.png")
 canvas.create_image(120,120, image=pomodoro)
 canvas.create_text(120,110,text="00:00",fill="white",font=("Arial",25,"bold"))
-canvas.pack()
 
 window.mainloop()
