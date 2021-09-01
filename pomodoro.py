4bae1020b80aae08bda8064556fab263ab7b86c1
Sumit Gaur <sumit.gaur1999@gmail.com>
2021-09-02 01:21:24 +0530

    added time function

--- pomodoro.py
+++ pomodoro.py
@@ -2,16 +2,31 @@ import tkinter
 from tkinter import *
 from tkinter import font
 import time
+import math
 
 FONT="Courier"
+START=5
 WORK_TIME=25
 SHORT_BREAK=5
 LONG_BREAK=10 
 
-def countdown(flag):
-    canvas.itemconfig(timer_text,text=flag)
-    while(flag>0):
-        window.after(1000,countdown,flag-1)
+def reset():
+    start_timer()
+
+def start_timer(flag):
+    
+    min=math.floor(flag/60)
+    sec=flag%60
+
+
+    canvas.itemconfig(timer_text,text=f"{min}:{sec}")
+    if(flag>0):
+        window.after(1000,start_timer,flag-1)
+    
+        
+ 
+def countdown():
+    start_timer(5*60)
 
 window=Tk()
 
@@ -20,6 +35,7 @@ window.config(padx=50,pady=50,bg="YELLOW")
 
 title_label=Label(text="Timer",fg="GREEN",bg="YELLOW")
 window.iconbitmap('tomato.ico')
+
 title_label.grid(column=1,row=0)
 title_label.config(font=('Helvatical bold',40))
 
@@ -29,8 +45,10 @@ pomodoro=PhotoImage(file="tomato.png")
 canvas.create_image(120,120, image=pomodoro)
 timer_text=canvas.create_text(120,110,text="00:00",fill="white",font=("Arial",25,"bold"))
 canvas.grid(column=1,row=2)
-countdown(60)
-start=Button(text="Start")
+
+
+
+start=Button(text="Start",command=countdown)
 start.grid(column=0,row=3)
 
 reset=Button(text="reset")
