bc38db8d213cea89c98eeb86926b98d49a481169
Sumit Gaur <sumit.gaur1999@gmail.com>
2021-09-01 21:15:39 +0530

    countdown

--- pomodoro.py
+++ pomodoro.py
@@ -6,8 +6,12 @@ import time
 FONT="Courier"
 WORK_TIME=25
 SHORT_BREAK=5
-LONG_BREAK=10
+LONG_BREAK=10 
 
+def countdown(flag):
+    canvas.itemconfig(timer_text,text=flag)
+    while(flag>0):
+        window.after(1000,countdown,flag-1)
 
 window=Tk()
 
@@ -19,11 +23,13 @@ window.iconbitmap('tomato.ico')
 title_label.grid(column=1,row=0)
 title_label.config(font=('Helvatical bold',40))
 
+
 canvas=Canvas(width=250,height=250)
 pomodoro=PhotoImage(file="tomato.png")
 canvas.create_image(120,120, image=pomodoro)
-canvas.create_text(120,110,text="00:00",fill="white",font=("Arial",25,"bold"))
+timer_text=canvas.create_text(120,110,text="00:00",fill="white",font=("Arial",25,"bold"))
 canvas.grid(column=1,row=2)
+countdown(60)
 start=Button(text="Start")
 start.grid(column=0,row=3)
 
@@ -33,5 +39,6 @@ reset.grid(column=3,row=3)
 
 #countdown mechanism
 
+         
 
 window.mainloop()
